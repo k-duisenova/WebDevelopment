07f2d5596325ddee31d09306962143262f41a34d
Kuralay <kuralaiduisenova97@gmail.com>
2020-03-04 21:06:39 +0600

    update

---
+++ week7/src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  // tslint:disable-next-line:max-line-length
+  { id: 11, name: 'Captain America', img_src: 'https://animatedtimes.com/wp-content/uploads/2019/11/Avengers-Endgame-Captain-America-Time-Travel-Explained.jpg' },
+  // tslint:disable-next-line:max-line-length
+  { id: 12, name: 'Wonder Woman' , img_src: 'https://cdn.shopify.com/s/files/1/1556/9595/products/WW_Cv750_UNKNOWN_ANACLETO_VIRGIN_1200x1200.jpg?v=1575935715'},
+  // tslint:disable-next-line:max-line-length
+  { id: 13, name: 'Supergirl' , img_src: 'https://storage.googleapis.com/stateless-www-monkeysfightingr/2019/09/ebb353bb-supergirl-34-feature-image.jpg'},
+  { id: 14, name: 'Batman' , img_src: 'https://i.pinimg.com/originals/8f/65/1a/8f651a6ec72dd4528e1f6a948b323558.jpg'},
+  // tslint:disable-next-line:max-line-length
+  { id: 15, name: 'The Human Torch' , img_src: 'https://www.strangearts.ru/sites/default/files/heroes/catalog/images/human_torch_billy_tan54cv.jpg'},
+  // tslint:disable-next-line:max-line-length
+  { id: 16, name: 'Spiderman' , img_src: 'https://cdn11.bigcommerce.com/s-0kvv9/images/stencil/1280x1280/products/306001/436169/apiibcz85__65409.1566749349.jpg?c=2&imbypass=on'},
+  { id: 17, name: 'Ironman' , img_src: 'https://i.pinimg.com/originals/69/21/84/69218426116a474d0180e6a1b8908258.jpg'},
+  { id: 18, name: 'Superman' , img_src: 'https://www.syfy.com/sites/syfy/files/styles/1200x680/public/2018/01/action-comics-cover_0.jpg'},
+  { id: 19, name: 'Storm', img_src: 'https://i.pinimg.com/originals/45/25/db/4525dbe50e3ee5030b09722749e8393c.jpg' },
+  { id: 20, name: 'Invisible woman', img_src: 'https://www.ultimatecomics.com/wp-content/uploads/2019/07/Photograph-352.jpg' }
+];
